1c54e1b8d35b6b5d07e1dee24d483e6902b21582
Anil Dhakal <adhakal321@gmail.com>
2023-02-14 09:13:57 +1300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,1 @@
-select schema_name from information_schema.schemata where schema_name <>'INFORMATION_SCHEMA';
+select schema_name from RAW_SANDBOX_SAMI.information_schema.schemata where schema_name <>'INFORMATION_SCHEMA';
