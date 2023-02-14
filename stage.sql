6e608c24c022cfa0e4e5f3721a8bc2e9b425c8a6
Anil Dhakal <adhakal321@gmail.com>
2023-02-14 15:08:31 +1300

    Update stage.sql

--- stage.sql
+++ stage.sql
@@ -1,1 +1,1 @@
-select 'ALTER STAGE '||STAGE_CATALOG ||'.'||STAGE_SCHEMA||'.'||STAGE_NAME ||' SET URL = ''' || REPLACE(stage_url,'s3://nzp-prd-s3-dataplatform','s3://nzp-npe-s3-dataplatform/dev') ||''',storage_integration =s3_integration_npe' as stage from DEV_RAW_SAMI.information_schema.stages ;
+select 'ALTER STAGE '||STAGE_CATALOG ||'.'||STAGE_SCHEMA||'.'||STAGE_NAME ||' SET URL = ''' || REPLACE(stage_url,'s3://nzp-prd-s3-dataplatform','s3://nzp-npe-s3-dataplatform/dev') ||''',storage_integration =s3_integration_npe;' as stage from DEV_RAW_SAMI.information_schema.stages ;
